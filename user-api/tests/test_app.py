6e41d37870394dc0e1d1752f0ee8d00ed5b1dc2c
Roque <lrvroque@gmail.com>
2020-07-27 17:13:14 -0300

    add test for get users from db

--- user-api/tests/test_app.py
+++ user-api/tests/test_app.py
@@ -1,5 +1,15 @@
-from src.app import app, save_to_db, User, db
+from src.app import app, save_to_db, get_users_from_db, User, db
 import pytest
+
+
+@pytest.fixture
+def user_created():
+    user = User(name="luiz")
+    db.session.add(user)
+    db.session.commit()
+    yield user
+    db.session.delete(user)
+    db.session.commit()
 
 
 def test_save_to_db_with_success():
@@ -8,4 +18,9 @@ def test_save_to_db_with_success():
     assert User.query.filter_by(name=name).count() == 1
     User.query.delete()
     db.session.commit()
+
+
+def test_get_users_from_db_with_success(user_created):
+    expected_user = user_created.name
+    assert expected_user in get_users_from_db()
 

daf53acf2d99f50a4a343533ae27bc8450315805
Roque <lrvroque@gmail.com>
2020-07-27 17:33:32 -0300

    fix testes

--- user-api/tests/test_app.py
+++ user-api/tests/test_app.py
@@ -2,16 +2,6 @@ from src.app import app, save_to_db, get_users_from_db, User, db
 import pytest
 
 
-@pytest.fixture
-def user_created():
-    user = User(name="luiz")
-    db.session.add(user)
-    db.session.commit()
-    yield user
-    db.session.delete(user)
-    db.session.commit()
-
-
 def test_save_to_db_with_success():
     name = "shuri"
     save_to_db(name)
@@ -20,8 +10,11 @@ def test_save_to_db_with_success():
     db.session.commit()
 
 
-def test_get_users_from_db_with_success(user_created):
-    expected_user = user_created.name
+def test_get_users_from_db_with_success():
+    expected_user = "shuri"
+    save_to_db(expected_user)
     user_list = get_users_from_db()
     assert expected_user in user_list
+    User.query.delete()
+    db.session.commit()
 

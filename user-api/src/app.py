e69d78965356267075a97f3b47e6dccc74f0a094
Roque <lrvroque@gmail.com>
2020-07-27 11:21:02 -0300

    add sql_alchemy config in the app

--- user-api/src/app.py
+++ user-api/src/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request
-import requests
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/user-api.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
 
 
 @app.route("/users", methods=["POST"])
@@ -9,3 +16,5 @@ def all_users():
     data = request.get_json()
 
     return data
+
+
